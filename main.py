45e0ee9397ac30230f4a22e2351d21d5cc345973
BambuUp1973 <danesin.mauro@gmail.com>
2026-03-14 11:24:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
+import os
 
+app = FastAPI()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+@app.get("/")
+def home():
+    return {"status": "BambuUp Bot running"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
